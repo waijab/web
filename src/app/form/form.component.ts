80905f1e4e2683ac7d4e1ecb8247ef177d078562
WAIJAB <jababbaew@gmail.com>
2019-02-17 15:34:55 +0700

    w4

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, EmailValidator } from '@angular/forms';
+import { FormBuilder, FormGroup, EmailValidator, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { user } from '../user';
 
 @Component({
@@ -16,18 +16,40 @@ export class FormComponent implements OnInit {
   
   ngOnInit() {
     this.formGroup = this.FormBuild.group({
-      firstName: this.FormBuild.control('supagrid'),
-      lastName: ['tangsermit'],
-      email:[''],
+      firstName: ['',[Validators.required,Validators.minLength(2)]],
+      lastName:['',[Validators.required,Validators.minLength(2)]],
+      email:['',[Validators.required,Validators.email]],
       age: [22]
     })
   }
+  EmilValidator(control: AbstractControl)
+{
+  const value = control.value;
+  if (value && value.includes('@')){
+    return null;
+  }
+  return {
+    email: true
+  }
+}
+  
  onSubmit(form: FormGroup){
+   console.log(form.valid, form.invalid)
+   console.log((<FormControl>form.get('firstName')).errors);
    
-   const {firstName, lastName, email, age} = form.value;
+   if (form.valid){
+     const {firstName, lastName, email, age} = form.value;
    const User = new user(firstName, lastName, email, age);
    console.log(User);
 
+     
+   }else{
+     ['firstName','lastName','age','email'].forEach((key: string)=> {
+       console.log(form.get(key).touched);
+       form.get(key).markAsTouched();
+     
+   })
 
+ }
  }
 }
